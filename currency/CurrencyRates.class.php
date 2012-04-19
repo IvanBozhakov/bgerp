18d735a5bd27acd6ad123b5d18ccd1ae50cf5660
Stefan Stefanov <stefan.bg@gmail.com>
2012-04-19 19:52:25 +0300

    Експеримент с новия пакет jqplot

--- currency/CurrencyRates.class.php
+++ currency/CurrencyRates.class.php
@@ -22,32 +22,64 @@ defIfNot('BGERP_BASE_CURRENCY', 'BGN');
  */
 class currency_CurrencyRates extends core_Manager
 {
-    
-    
+
+
     /**
      * Плъгини за зареждане
      */
-    var $loadList = 'plg_Created, plg_RowTools, Currencies=currency_Currencies, currency_Wrapper, plg_Sorting, plg_Chart';
-    
-    
+    var $loadList = 'plg_Created, plg_RowTools, Currencies=currency_Currencies, currency_Wrapper, plg_Sorting, jqplot_Plugin';
+
+
     /**
      * Полета, които ще се показват в листов изглед
      */
     var $listFields = "currencyId, date, rate, baseCurrencyId";
-    
-    
+
+
     /**
      * Заглавие
      */
     var $title = 'Исторически валутни курсове';
-    
-    
+
+
     /**
      * Брой записи на страница
      */
     var $listItemsPerPage = 500;
-    
-    
+
+
+    static $charts = array(
+        'rate_in_time_lines' => array(
+            'menu' => 'По валути (линии)',
+            'type' => 'lines',
+            'diff' => 'currencyId',
+            'titleTpl' => 'Цена за 1 [#baseCurrencyId#] в [#currencyId#]',
+            'labelTpl' => '[#rate#] [#currencyId#]',
+            'ax' => 'date',
+            'ay' => 'rate'
+        ),
+        'rate_in_time_bars' => array(
+            'menu' => 'По валути (площ)',
+            'type' => 'bars',
+            'diff' => 'currencyId',
+            'titleTpl' => 'Цена за 1 [#baseCurrencyId#] в [#currencyId#]',
+            'labelTpl' => '[#rate#] [#currencyId#]',
+            'ax' => 'date',
+            'ay' => 'rate'
+        ),
+        'all' => array(
+            'menu' => 'Всички',
+            'series' => 'currencyId',
+            'diff' => 'date',
+            'type' => 'bars',
+//             'titleTpl' => 'Цена за 1 [#baseCurrencyId#] в [#currencyId#]',
+            'labelTpl' => '[#rate#] [#currencyId#]',
+            'ax' => 'date',
+            'ay' => 'currencyId'
+        ),
+    );
+
+
     /**
      * Описание на модела (таблицата)
      */
@@ -57,11 +89,11 @@ class currency_CurrencyRates extends core_Manager
         $this->FLD('baseCurrencyId', 'key(mvc=currency_Currencies, select=code)', 'caption=Към основна валута');
         $this->FLD('date', 'date', 'caption=Курс->дата,chart=ax');
         $this->FLD('rate', 'double', 'caption=Курс->стойност,chart=ay');
-        
+
         $this->setDbUnique('currencyId,baseCurrencyId,date');
     }
-    
-    
+
+
     /**
      * Зареждане на валути от xml файл от ECB
      *
@@ -70,27 +102,27 @@ class currency_CurrencyRates extends core_Manager
     function retrieveCurrenciesFromEcb()
     {
         $euroId = $this->Currencies->fetchField("#code='EUR'", 'id');
-        
+
         $this->data->rates = array();
         $XML = simplexml_load_file("http://www.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml");
         $now = $XML->Cube->Cube['time']->__toString();
-        
+
         $countCurrencies = 0;
-        
+
         foreach($XML->Cube->Cube->Cube as $item){
             $rate = $item['rate']->__toString();
             $currency = $item['currency']->__toString();
-            
+
             // $currencyId = $this->Currencies->fetchField("#code='{$currency}'", 'id');
             $currencyId = $this->Currencies->fetchField(array("#code='[#1#]'", $currency), 'id');
-            
+
             if(!$currencyId) continue;
-            
+
             $state = $this->Currencies->fetchField($currencyId, "state");
-            
+
             if ($state == "closed") continue;
-            
-            // Проверка дали имаме такъв запис за текуща дата 
+
+            // Проверка дали имаме такъв запис за текуща дата
             if ($this->fetch("#currencyId={$currencyId} AND #baseCurrencyId={$euroId} AND #date='{$now}'")) {
                 continue;
             }
@@ -99,29 +131,29 @@ class currency_CurrencyRates extends core_Manager
             $rec->baseCurrencyId = $euroId;
             $rec->date = $now;
             $rec->rate = $rate;
-            
+
             $currenciesRec = new stdClass();
             $currenciesRec->id = $rec->currencyId;
             $currenciesRec->lastUpdate = $rec->date;
             $currenciesRec->lastRate = $rec->rate;
-            
+
             $this->Currencies->save($currenciesRec, 'lastUpdate,lastRate');
-            
+
             $this->save($rec);
-            
+
             $countCurrencies++;
         }
-        
+
         if($countCurrencies == '0') {
             $res = "Няма нови курсове за валути.";
         } else {
             $res = "Извлечени са курсове за {$countCurrencies} валути.";
         }
-        
+
         return $res;
     }
-    
-    
+
+
     /**
      * Метод за Cron за зареждане на валутите
      */
@@ -129,8 +161,8 @@ class currency_CurrencyRates extends core_Manager
     {
         return $this->retrieveCurrenciesFromEcb();
     }
-    
-    
+
+
     /**
      * Action за тестване зареждането на валутите в debug mode.
      * В production mode този метод не се използва.
@@ -139,8 +171,8 @@ class currency_CurrencyRates extends core_Manager
     {
         return new Redirect (array('currency_CurrencyRates', 'default'), $this->retrieveCurrenciesFromEcb());
     }
-    
-    
+
+
     /**
      * Зареждане на Cron задачите за валутите след setup на класа
      *
@@ -150,7 +182,7 @@ class currency_CurrencyRates extends core_Manager
     static function on_AfterSetupMvc($mvc, &$res)
     {
         $Cron = cls::get('core_Cron');
-        
+
         $rec = new stdClass();
         $rec->systemId = "update_currencies_afternoon";
         $rec->description = "Зарежда валутни курсове";
@@ -159,17 +191,17 @@ class currency_CurrencyRates extends core_Manager
         $rec->period = 24 * 60;
         $rec->offset = 17 * 60;
         $Cron->addOnce($rec);
-        
+
         unset($rec->id);
         $rec->systemId = "update_currencies_night";
         $rec->offset = 21 * 60;
-        
+
         $Cron->addOnce($rec);
-        
+
         $res .= "<li style='color:#660000'>На Cron са зададени update_currencies_afternoon и update_currencies_night</li>";
     }
-    
-    
+
+
     /**
      * Извиква се след подготовката на toolbar-а за табличния изглед
      */
